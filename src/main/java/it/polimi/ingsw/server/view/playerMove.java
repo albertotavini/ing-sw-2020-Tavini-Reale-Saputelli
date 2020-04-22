5ce6dc43ce186e9a663dc597f056d41381d148ef
Alberto Tavini <10578559@polimi.it>
2020-04-22 16:54:18 +0200

    changing some things in PlayerMove

--- src/main/java/it/polimi/ingsw/server/view/playerMove.java
+++ src/main/java/it/polimi/ingsw/server/view/playerMove.java
@@ -8,11 +8,31 @@ public class playerMove {
     private int column;
     private Player player;
     private String genericMessage;
+    private Boolean confirmation;
 
     public playerMove(int row, int column, Player player) {
         this.row = row;
         this.column = column;
         this.player = player;
+        this.genericMessage = "nothing interesting here";
+        this.confirmation = false;
+    }
+
+    public playerMove(String string, Player player) {
+        this.player = player;
+        this.column = 7;
+        this.row = 7;
+        this.genericMessage = string;
+        this.confirmation = false;
+    }
+
+    public playerMove (Boolean confirmation, Player player) {
+        this.player = player;
+        this.column = 7;
+        this.row = 7;
+        this.genericMessage = "nothing interesting here";
+        this.confirmation = confirmation;
+
     }
 
     public int getRow() {
