116a0e8d01401a0eb1485c2246e65e56e47c0092
Alberto Tavini <10578559@polimi.it>
2020-05-16 16:16:52 +0200

    fixing sonar warnings in Distributed View

--- src/main/java/it/polimi/ingsw/server/view/DistributedView.java
+++ src/main/java/it/polimi/ingsw/server/view/DistributedView.java
@@ -1,26 +1,21 @@
 package it.polimi.ingsw.server.view;
 
-import it.polimi.ingsw.server.model.Board;
 import it.polimi.ingsw.server.model.BoardPhotography;
 import it.polimi.ingsw.server.model.Player;
 import it.polimi.ingsw.server.observers.ModelMessage.ModelMessage;
-import it.polimi.ingsw.server.observers.ModelMessage.ModelMessageType;
 import it.polimi.ingsw.server.observers.Observable;
 import it.polimi.ingsw.server.observers.Observer;
-import it.polimi.ingsw.server.view.PlayerMove.ConfirmationEnum;
 import it.polimi.ingsw.server.view.PlayerMove.PlayerMove;
 
 public abstract class DistributedView extends Observable <PlayerMove> implements Observer<BoardPhotography> {
     private Player player;
-    private ConfirmationEnum confirmation;
-    private boolean done = false;
     ModelMessage currentModelMessage;
 
     DistributedView(Player player) {
         this.player = player;
     }
 
-    protected Player getPlayer() {
+    Player getPlayer() {
         return player;
     }
 
